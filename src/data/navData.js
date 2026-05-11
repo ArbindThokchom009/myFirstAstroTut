6527cbbee02d6c4f928e305de33d62bfef18eb85
thokchomarbind009 <thokchomarbind009@mail.com>
2026-05-11 16:54:06 +0530

    feature-adding

---
+++ src/data/navData.js
@@ -0,0 +1,16 @@
+const navData = [
+    {
+        name: "Home",
+        link: "/"
+    },
+    {
+        name: "About",
+        link: "/about"
+    },
+    {
+        name: "Blog",
+        link: "/blog"
+    }
+]
+
+export default navData;
